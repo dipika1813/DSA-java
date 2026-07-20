4cdf44d20f0e067ef5cdb39c3641333adc0607d8
Dipika1813 <pawardipika24@gmail.com>
2026-07-20 23:19:31 +0530

    new changes

---
+++ Conditionals and loops/Reverse.java
@@ -0,0 +1,17 @@
+public class Reverse{
+    public static void main(String[] args) {
+        int n = 28479;
+
+        int ans = 0;
+
+        while(n > 0){
+            int rem = n % 10;
+            // n = n/10;
+
+            ans = ans * 10 + rem;
+        }
+
+        System.out.println(ans);
+
+    }
+}
