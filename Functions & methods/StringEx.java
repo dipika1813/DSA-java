d2fb51cb1aceb60fa6caebf91e21d186168f2f9e
Dipika1813 <pawardipika24@gmail.com>
2026-07-26 23:11:21 +0530

    new update

---
+++ Functions & methods/StringEx.java
@@ -0,0 +1,14 @@
+public class StringEx{
+    public static void main(String[] args) {
+        greet();
+        System.out.println(greet());
+
+    }
+    static String greet(){
+
+        String greeting = "hello";
+        return greeting;
+
+
+    }
+}
