3f1f67ef7b30081c3c644d24a7baf31d910999f4
Dipika1813 <pawardipika24@gmail.com>
2026-08-10 23:57:31 +0530

    new update

--- Arrays & ArrayList/Swap.java
+++ Arrays & ArrayList/Swap.java
@@ -3,8 +3,9 @@ import java.util.Arrays;
 public class Swap{
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,5};
-        swap(arr, 3, 4);
 
+        // swap(arr, 3, 4);
+        Reverse(arr);
         System.out.println(Arrays.toString(arr));
 
     }
@@ -13,9 +14,20 @@ public class Swap{
         arr[idx1] = arr[idx2];
         arr[idx2] = temp;
     }
+    static void Reverse(int[] arr){
+        int start = 0; 
+        int end = arr.length-1;
+        
+        while(start < end){
+            //swap
+            swap(arr, start , end);
+            start ++;
+            end--;
+        }
         
 
 
 
 
     }
+}
